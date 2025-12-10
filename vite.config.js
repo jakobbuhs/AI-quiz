55898f8dfe83eccc6562b3c295cfc2f2960b1d6b
jakobbuhs <jakob@buhs.no>
2025-12-10 17:04:25 +0100

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // IMPORTANT: Change 'AI-quiz' to your GitHub repository name
-  base: '/AI-quiz/',
+  base: '/',
 })
 
